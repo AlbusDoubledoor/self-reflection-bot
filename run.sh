115208b6e8d62b3c87e4426781a287e9b84bacc9
Roman Koval <kovalre@yahoo.com>
2025-06-16 01:22:12 +0200

    run.sh updated, since logger is added

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 # Load .env variables (ignore comments and blank lines)
 export $(grep -v '^#' local.env | xargs)
 
-# Run the jar in the background, pipe stdout and stderr to log file
-nohup java -jar selfreflectionbot-1.0.0.jar >> out.log 2>&1 &
+# Run the jar, pipe stdout and stderr to log file
+nohup java -jar selfreflectionbot-1.0.0.jar >/dev/null 2>&1 &
